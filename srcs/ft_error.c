7002b045edf8bb4a560014d4878994fca79fefbd
Mickael SORIN <msorin@e3r9p9.42.fr>
2017-11-12 11:17:29 +0100

    "secu anti-leaks"

--- srcs/ft_error.c
+++ srcs/ft_error.c
@@ -20,18 +20,23 @@ void	ft_error_init(void)
 	exit(1);
 }
 
+/*
+** Certes, clear_ls n'est peut-etre pas indispensable,
+** mais c'est sur le principe de la guerre aux leaks.
+*/
+
 void	ft_error_malloc(t_ls *ls)
 {
-	(void)ls;
-	write(2, "A memory allocation failde\n", 27);
+	write(2, "A memory allocation failed\n", 27);
+	clear_ls(ls);
 	exit(1);
 }
 
 void	ft_error_illegal_opt(char f, t_ls *ls)
 {
-	(void)ls;
 	write(2, "ls: illegal option -- ", 22);
 	write(2, &f, 1);
 	write(2, "\nusage: ls [-Ralrt] [file ...]\n", 31);
+	clear_ls(ls);
 	exit(2);
 }
