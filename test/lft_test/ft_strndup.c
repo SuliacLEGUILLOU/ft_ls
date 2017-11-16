18d76e99e41084fd4aa7aab8e9cabc1cff96567e
Mickael SORIN <msorin@e3r9p9.42.fr>
2017-11-16 11:47:01 +0100

    ajout de fonction utilisable dans libft

---
+++ test/lft_test/ft_strndup.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strndup.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msorin <msorin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/16 11:40:52 by msorin            #+#    #+#             */
+/*   Updated: 2017/11/16 11:45:00 by msorin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft>
+
+char	*ft_strndup(char *src, int len)
+{
+	int		i;
+	int		min;
+	char	*dest;
+
+	min = ft_min(ft_strlen(src), len);
+	if (!(dest = ft_strnew(min)))
+		return (NULL);
+	i = 0;
+	while (i < min)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
