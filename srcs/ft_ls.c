27b666f0f71b53d80057d094603e11e0b155ed20
Suliac <s18alg@protonmail.com>
2017-11-16 18:31:13 +0100

    got to go

--- srcs/ft_ls.c
+++ srcs/ft_ls.c
@@ -6,7 +6,7 @@
 /*   By: sle-guil <sle-guil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/30 16:51:17 by msorin            #+#    #+#             */
-/*   Updated: 2017/11/16 17:54:07 by sle-guil         ###   ########.fr       */
+/*   Updated: 2017/11/16 18:31:00 by sle-guil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,33 @@
 ** else print with (flag)
 */
 
+static void	st_fill_struct_dir(t_doc **arg, int i, DIR *dir)
+{
+	struct dirent	*info;
+
+	(void)arg;
+	(void)i;
+	(void)dir;
+}
+
 int        ft_ls(t_ls *ls)
 {
-    for (int i = 0; ls->arg[i]; i++) {
-        ft_putendl(ls->arg[i]->name);
+	int		i;
+	DIR		*dir_info;
+
+	i = 0;
+	while (ls->arg[i])
+	{
+		if (S_IFDIR & ls->arg[i]->stat->st_mode)
+		{
+			// is a dir
+			dir_info = opendir(ls->arg[i]->name);
+			st_fill_struct_dir(ls->arg, i, dir_info);
+		}
+		else
+		{
+			ft_putendl(ls->arg[i]->name);
+		}
 	}
     return (ls->error);
 }
