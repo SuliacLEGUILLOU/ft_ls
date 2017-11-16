1f878dae030e9725fff4e64b38873521e365e9db
Mickael SORIN <msorin@e3r9p9.42.fr>
2017-11-16 11:40:39 +0100

    et une fonction de plus qui peut etre ajoute a la libft

--- test/ft_strreplace_str.c
+++ test/ft_strreplace_str.c
@@ -12,51 +12,88 @@
 
 #include <libft.h>
 
-int		count_char(char *str, char c)
+int		count_replace(char *str, char *pat, char c)
 {
 	int		i;
 	int		ret;
+	char	*t1;
+	char	*t2;
 
-	if (!c)
-		return (1);
-	i = -1;
+	i = 0;
 	ret = 0;
-	while (str[++i])
+	t1 = str;
+	while (t1[i])
 	{
-		if (str[i] == c)
+		if ((t2 = ft_strnstr(str, pat, ft_strlen(pat))))
+		{
 			ret++;
+			i += ft_strlen(pat) - 1;
+		}
+		else if (t1[i] == c)
+			ret++;
+		i++;
 	}
 	return (ret);
 }
 
-int		count_patern(char *src, char *pat)
+void	fill_tab(char ***tmp, char *str, char *pat, char c)
 {
-	int		ret;
-	char	*t;
+	int		i;
+	int		j;
+	char	*t1;
+	char	*t2;
 
-	if (!(*pat))
-		return (1);
-	ret = 0;
-	t = src;
-	while ((t = ft_strstr(t, pat)))
+	i = -1;
+	j = 0;
+	t1 = str;
+	t2 = NULL;
+	while (t1[++i])
 	{
-		t += ft_strlen(pat);
-		ret++;
+		if ((t2 = ft_strnstr(str, pat, ft_strlen(pat))) || t1[i] == c)
+		{
+			(*tmp)[j] = ft_strndup(t1, i);
+			if (t2)
+				t1 += i + ft_strlen(pat);
+			else
+				t1 += i + 1;
+			t2 = NULL;
+			i = -1;
+			j++;
+		}
 	}
-	return (ret);
+	(*tmp)[j] = ft_strdup(t1);
+	(*tmp)[j + 1] = NULL;
 }
 
-char	*ft_strreplace_str(char *s, char *p, char *c, char *r)
+char	*ft_strjoin_tab(char ***tab, char *rep)
+{
+	int		i;
+	char	*str;
+
+	str = ft_strdup((*tab)[0]);
+	free((*tab)[0]);
+	i = 1;
+	while ((*tab)[i])
+	{
+		str = ft_strjoin_f(str, rep, 1);
+		str = ft_strjoin_f(str, (*tab)[i], 3);
+		i++;
+	}
+	free(*tab);
+	return (str);
+}
+
+char	*ft_strreplace_str(char *str, char *pat, char *c, char *rep)
 {
 	int		n;
-	char	*d;
+	char	*dest;
+	char	**tmp;
 
-	if (!s || !r || (!p && !c))
+	if (!str || !rep || (!pat && !c))
 		return (NULL);
-	if (!p)
-		n = count_char(s, *c)
-	else
-		n = count_patern(s, p);
-	d = malloc(sizeof(char) * (ft_strlen(s) + n * (ft_strlen(r) - 1) + 1));
-	//
+	n = count_replace(str, pat, *c);
+	tmp = (char**)malloc(sizeof(char*) * (n + 2));
+	fill_tab(&tmp, str, pat, *c);
+	dest = ft_strjoin_tab(&tmp, rep);
+	return (dest);
 }
